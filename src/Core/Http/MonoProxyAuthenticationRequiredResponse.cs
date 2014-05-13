514b5f739272ac6e89cebedd86ff2e9b73979a60
Matt Ward <ward.matt@gmail.com>
2014-05-13 15:20:00 +0100

    Fix proxy authentication failing on Mono.
    When a system wide proxy is configured for all https requests then a
    requesting a url such as https://www.nuget.org/api/v2 would fail to
    request proxy credentials on Mono so the user would not be prompted.
    Instead the request would be retried 10 times and then fail.
    
    This only affects https requests. A request made to a http url such
    as http://www.nuget.org/api/v2 would request the proxy credentials
    from the credential provider.
    
    With Microsoft's .NET Framework a WebException is thrown with:
    
        WebException.Status = ProtocolError
        WebException.Response.StatusCode = ProxyAuthenticationRequired
    
    Exception call stack:
    
    System.Net.WebException:
        The remote server returned an error: (407) Proxy
    Authentication Required.
       at System.Net.HttpWebRequest.GetResponse()
    
    On Mono a WebException is thrown with:
    
        WebException.Status = SecureChannelFailure
        WebException.Response = null
    
    Exception call stack:
    
    System.Net.WebException: Error getting response stream (The remote server
    returned a 407 status code.): SecureChannelFailure ---> System.Exception:
        at
    System.Net.WebConnection.HandleError(WebExceptionStatus st, System.Exception e,
    System.String where)
       at System.Net.WebConnection.CreateTunnel(System.Net.HttpWebRequest request,
    System.Uri connectUri, System.IO.Stream stream, System.Byte[] ByRef buffer)
       at System.Net.WebConnection.CreateStream(System.Net.HttpWebRequest request)
       at System.Net.WebConnection.InitConnection(System.Object state)
       at System.Net.WebConnection.<WebConnection>m__20(System.Object state)
      at System.Net.WebConnection.HandleError (WebExceptionStatus st,
    System.Exception e, System.String where) [0x00000] in <filename unknown>:0
    
    To handle this there is now a special case which checks the exception
    message for the string 'The remote server returned a 407 status code.'
    when running on Mono.

---
+++ src/Core/Http/MonoProxyAuthenticationRequiredResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Net;
+
+namespace NuGet
+{
+    class MonoProxyAuthenticationRequiredResponse : IHttpWebResponse
+    {
+        public MonoProxyAuthenticationRequiredResponse ()
+        {
+            Headers = new NameValueCollection ();
+            StatusCode = HttpStatusCode.ProxyAuthenticationRequired;
+        }
+
+        public void Dispose ()
+        {
+        }
+
+        public string AuthType { get; private set; }
+        public NameValueCollection Headers { get; private set; }
+        public Uri ResponseUri { get; private set; }
+        public HttpStatusCode StatusCode { get; private set; }
+    }
+}
